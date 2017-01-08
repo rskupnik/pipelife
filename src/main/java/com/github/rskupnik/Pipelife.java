e4d00c2c271fedf6b3ea26cc179e9fb828e7dd18
Myzreal <r.skupnik@gmail.com>
2017-01-08 14:54:13 +0100

    Add Evernote initialization :sparkles:

--- src/main/java/com/github/rskupnik/Pipelife.java
+++ src/main/java/com/github/rskupnik/Pipelife.java
@@ -1,5 +1,10 @@
 package com.github.rskupnik;
 
+import com.evernote.auth.EvernoteAuth;
+import com.evernote.auth.EvernoteService;
+import com.evernote.clients.ClientFactory;
+import com.evernote.clients.NoteStoreClient;
+import com.evernote.clients.UserStoreClient;
 import com.github.rskupnik.parrot.Parrot;
 
 public final class Pipelife {
@@ -13,6 +18,8 @@ public final class Pipelife {
     private final String EVERNOTE_TOKEN;
 
     private final Parrot config;
+    private UserStoreClient userStore;
+    private NoteStoreClient noteStore;
 
     private Pipelife() {
         config = new Parrot();
@@ -24,6 +31,41 @@ public final class Pipelife {
         TRELLO_KEY = config.get(CONFIG_TRELLO_KEY).orElseThrow(IllegalStateException::new);
         TRELLO_TOKEN = config.get(CONFIG_TRELLO_TOKEN).orElseThrow(IllegalStateException::new);
         EVERNOTE_TOKEN = config.get(CONFIG_EVERNOTE_TOKEN).orElseThrow(IllegalStateException::new);
+
+        try {
+            initializeEvernote();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        getNotes();
+    }
+
+
+    /**
+     * Initializes connection to Evernote
+     */
+    private void initializeEvernote() throws Exception {
+        EvernoteAuth evernoteAuth = new EvernoteAuth(EvernoteService.PRODUCTION, EVERNOTE_TOKEN);
+        ClientFactory clientFactory = new ClientFactory(evernoteAuth);
+        userStore = clientFactory.createUserStoreClient();
+
+        boolean versionOk = userStore.checkVersion("Pipelife",
+                com.evernote.edam.userstore.Constants.EDAM_VERSION_MAJOR,
+                com.evernote.edam.userstore.Constants.EDAM_VERSION_MINOR);
+        if (!versionOk) {
+            System.err.println("Incompatible Evernote client protocol version");
+            System.exit(1);
+        }
+
+        noteStore = clientFactory.createNoteStoreClient();
+    }
+
+    /**
+     * Receives all notes from Evernote
+     */
+    private void getNotes() {
+
     }
 
     private boolean isConfigValid() {
