dd3673f9aaa48ca5981a7b33cbe6737d26a2a0f8
Myzreal <r.skupnik@gmail.com>
2017-01-14 13:30:41 +0100

    Read notes from Evernote input buffer notebook :sparkles:

--- src/main/java/com/github/rskupnik/Pipelife.java
+++ src/main/java/com/github/rskupnik/Pipelife.java
@@ -5,17 +5,29 @@ import com.evernote.auth.EvernoteService;
 import com.evernote.clients.ClientFactory;
 import com.evernote.clients.NoteStoreClient;
 import com.evernote.clients.UserStoreClient;
+import com.evernote.edam.error.EDAMNotFoundException;
+import com.evernote.edam.error.EDAMSystemException;
+import com.evernote.edam.error.EDAMUserException;
+import com.evernote.edam.notestore.NoteFilter;
+import com.evernote.edam.type.Note;
+import com.evernote.edam.type.NoteSortOrder;
+import com.evernote.edam.type.Notebook;
+import com.evernote.thrift.TException;
 import com.github.rskupnik.parrot.Parrot;
+
+import java.util.List;
 
 public final class Pipelife {
 
     private static final String CONFIG_TRELLO_KEY = "trello_key";
     private static final String CONFIG_TRELLO_TOKEN = "trello_token";
     private static final String CONFIG_EVERNOTE_TOKEN = "evernote_token";
+    private static final String CONFIG_INPUT_NOTEBOOK = "input_notebook";
 
     private final String TRELLO_KEY;
     private final String TRELLO_TOKEN;
     private final String EVERNOTE_TOKEN;
+    private final String INPUT_NOTEBOOK;
 
     private final Parrot config;
     private UserStoreClient userStore;
@@ -31,16 +43,19 @@ public final class Pipelife {
         TRELLO_KEY = config.get(CONFIG_TRELLO_KEY).orElseThrow(IllegalStateException::new);
         TRELLO_TOKEN = config.get(CONFIG_TRELLO_TOKEN).orElseThrow(IllegalStateException::new);
         EVERNOTE_TOKEN = config.get(CONFIG_EVERNOTE_TOKEN).orElseThrow(IllegalStateException::new);
+        INPUT_NOTEBOOK = config.get(CONFIG_INPUT_NOTEBOOK).orElseThrow(IllegalStateException::new);
 
         try {
             initializeEvernote();
+            List<Note> notes = getNotes();
+            for (Note note : notes) {
+                System.out.println(note.getTitle() + ": "+note.getContent());
+            }
         } catch (Exception e) {
             e.printStackTrace();
             System.exit(-1);
         }
-        getNotes();
     }
-
 
     /**
      * Initializes connection to Evernote
@@ -62,19 +77,35 @@ public final class Pipelife {
     }
 
     /**
-     * Receives all notes from Evernote
+     * Receives all notes from Evernote from the specified input notebook
      */
-    private void getNotes() {
+    private List<Note> getNotes() throws TException, EDAMUserException, EDAMSystemException, EDAMNotFoundException {
+        Notebook inputNotebook = null;
+        for (Notebook notebook : noteStore.listNotebooks()) {
+            if (notebook.getName().equals(INPUT_NOTEBOOK)) {
+                inputNotebook = notebook;
+                break;
+            }
+        }
 
+        if (inputNotebook == null)
+            throw new IllegalStateException("Input notebook not found: "+INPUT_NOTEBOOK);
+
+        NoteFilter filter = new NoteFilter();
+        filter.setNotebookGuid(inputNotebook.getGuid());
+        filter.setOrder(NoteSortOrder.CREATED.getValue());
+        filter.setAscending(true);
+
+        return noteStore.findNotes(filter, 0, 100).getNotes();
     }
 
     private boolean isConfigValid() {
         if (config == null)
             return false;
 
-        String[] configEntries = new String[] {CONFIG_TRELLO_KEY, CONFIG_TRELLO_TOKEN, CONFIG_EVERNOTE_TOKEN};
+        String[] configEntries = new String[] {CONFIG_TRELLO_KEY, CONFIG_TRELLO_TOKEN, CONFIG_EVERNOTE_TOKEN, CONFIG_INPUT_NOTEBOOK};
         for (String configEntry : configEntries) {
-            if (!config.get(configEntry).isPresent()) {
+            if (!config.get(configEntry).isPresent() || config.get(configEntry).equals("")) {
                 System.err.println("Missing config entry: "+configEntry);
                 return false;
             }
